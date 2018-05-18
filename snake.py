8c45e8255c557e9b6d0280794f2ef98c3c8724b4
Wojciech Wozniak <wojciechw@kainos.com>
2018-05-18 22:28:22 +0200

    food spawns

--- snake.py
+++ snake.py
@@ -2,29 +2,96 @@ import pygame, sys, os, time, random
 
 
 pygame.init()
+pygame.mixer.init()
 
 width = 800
 height = 800
 screen = pygame.display.set_mode((width, height))
+
+#object loads
 snake_unit = pygame.image.load("img1")
-snake_food = pygame.image.load("img2")
+snake_body_img = pygame.image.load("img2")
+snake_food = pygame.image.load("img3")
+sound1 = pygame.mixer.Sound("sound1.wav")
+
 # width, height
 snake_head_location = (0, 0)
+food_location = (999, 999)
 current_move_direction = ""
 food_spawned = 0
+snake_body = list()
 
 screen.fill((100, 100, 100))
 screen.blit(snake_unit, (0, 0))
 
-#pygame.display.flip()
-#time.sleep(1)
-def draw_board():
-	screen.fill((100, 100, 100))
-	pygame.display.flip()
+def show_snake_body():
+	#global snake_body
+	#new_snake_body = list()
+	if len(snake_body) > 0:
+		'''if move_direction == "right":
+			for unit in snake_body:
+				new_unit = (unit[0] + 20, unit[1])
+				new_snake_body.append(new_unit)
+
+
+
+		#snake_body[0] = snake_head_location
+		for unit in new_snake_body:
+			screen.blit(snake_body_img, unit)
+		pygame.display.flip()
+		
+		#print(len(snake_body))
+		#snake_body[0] = snake_head_location
+		#for i in range(1, len(snake_body), 1):
+		#	snake_body[i] = snake_body[i-1] 
+		#snake_body[0] = snake_head_location
+
+		#for unit in snake_body:
+			#screen.blit(snake_body_img, unit)
+		#pygame.display.flip()
+	'''
+		snake_body[0] = snake_head_location
+		screen.blit(snake_body_img, snake_body[0])
+
+def enlarge_snake(head_x, head_y):
+	global snake_body
+	#snake_head_location[0] = head_x
+	#snake_head_location[1] = head_y
+	if len(snake_body) == 0:
+		new_body_unit = (head_x - 20, head_y)
+		snake_body.append(new_body_unit)
+	#else:
+		print(snake_body[0])
+		#snake_body[len(snake_body)-1]
+	
+	#for unit in snake_body:
+		#screen.blit(snake_unit, unit)
+		#pygame.display.flip()
+
+
+
+
+
+def food_eaten_check():
+	global food_spawned
+	head_x = snake_head_location[0]
+	head_y = snake_head_location[1]
+	food_x = food_location[0]
+	food_y = food_location[1]
+
+	for x in range(head_x, head_x + 21, 1):
+		for y in range(head_y, head_y + 21, 1):
+			if x == food_x and y == food_y:
+				print("food eaten")
+				food_spawned = 0
+				sound1.play()
+				enlarge_snake(head_x, head_y)
+				break
 
 
 def spawn_snake_food():
-	food_location = (random.randrange(0, 780, 10), random.randrange(0, 780, 10))
+	global food_location
+	food_location = (random.randrange(0, 780, 20), random.randrange(0, 780, 20))
 	#print(food_location)
 	screen.blit(snake_food, food_location)
 	return food_location
@@ -32,8 +99,9 @@ def spawn_snake_food():
 
 def move_snake_head(direction):
 	global snake_head_location
+	#print("move snake head")
 	#screen.fill((100, 100, 100))
-	move_unit = 10
+	move_unit = 20
 	#new_location = (5, 0)
 	#print(snake_head_location[0])
 	#print(snake_head_location[1])
@@ -71,7 +139,9 @@ def move_snake_head(direction):
 	pygame.display.flip()
 
 def input(events):
+	#print("input")
 	for event in events:
+		#print(event)
 		if event.type == pygame.QUIT:
 			sys.exit(0)
 		elif event.type == pygame.KEYDOWN:
@@ -92,10 +162,14 @@ def input(events):
 
 while True:
 	#draw_board()
-	screen.fill((100, 100, 100))
+	#screen.fill((100, 100, 100))
+	#food_eaten_check()
+	#show_snake_body(current_move_direction)
+	food_eaten_check()
 	moving_direction = input(pygame.event.get())
 	#print(moving_direction)
 	#if not food_spawned:
+	screen.fill((100, 100, 100))
 	if not food_spawned:
 		food_location = spawn_snake_food()
 		food_spawned = 1
@@ -104,6 +178,10 @@ while True:
 	if moving_direction:
 		current_move_direction = moving_direction
 		#spawn_snake_food()
+	#screen.fill((100, 100, 100))
 	move_snake_head(current_move_direction)
+	show_snake_body()
+	#show_snake_body()
+	#move_snake_head(current_move_direction)
 	pygame.display.flip()
-	#time.sleep(0.5)
+	#time.sleep(0.3)
